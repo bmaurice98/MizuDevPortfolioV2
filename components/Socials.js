cbe253d7a2255daf5f21bf315dc55bfbe65fd792
Brandon <bmaurice100@gmail.com>
2022-07-15 18:22:20 -0700

    social icons hover animation

--- components/Socials.js
+++ components/Socials.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React from "react";
 import Link from "next/link";
 import { DiGithubAlt } from "@react-icons/all-files/di/DiGithubAlt";
 import { SiLinkedin } from "@react-icons/all-files/si/SiLinkedin";
 import { SiLeetcode } from "@react-icons/all-files/si/SiLeetcode";
 import { SiSpotify } from "@react-icons/all-files/si/SiSpotify";
 import { SiTwitch } from "@react-icons/all-files/si/SiTwitch";
+import styled, { css } from "styled-components";
 
+const Icon = styled.div`
+  transform: translate(0%);
+  transition: 0.3s ease-out;
+
+  ${(props) =>
+    props.animated &&
+    css`
+      &:hover {
+        position: fixed;
+        transform: translate(0%, -30%);
+        transition: 0.3s ease-out;
+      }
+    `}
+`;
 
 export const Socials = () => {
   return (
     <div>
       <div>
-        <Link href={"https://github.com/bmaurice98"}>
-          <DiGithubAlt />
-        </Link>
+        <Icon animated>
+          <Link href={"https://github.com/bmaurice98"}>
+            <DiGithubAlt />
+          </Link>
+        </Icon>
         <Link href={"https://www.linkedin.com/in/brandon-maurice-a77190187/"}>
           <SiLinkedin />
         </Link>
@@ -29,4 +46,4 @@ export const Socials = () => {
       </div>
     </div>
   );
-}
+};
