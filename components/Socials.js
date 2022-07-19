c909d2415b83772b5cb880aeaede21e52b449591
Brandon <bmaurice100@gmail.com>
2022-07-18 23:09:37 -0700

    link rel fixes

--- components/Socials.js
+++ components/Socials.js
@@ -5,7 +5,7 @@ import { SiLinkedin } from "@react-icons/all-files/si/SiLinkedin";
 import { SiLeetcode } from "@react-icons/all-files/si/SiLeetcode";
 import { SiSpotify } from "@react-icons/all-files/si/SiSpotify";
 import { SiTwitch } from "@react-icons/all-files/si/SiTwitch";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Icons = styled.div`
   display: flex;
@@ -21,36 +21,56 @@ const Icons = styled.div`
     margin: 0.2rem 0;
   }
   & > *:hover {
-    color: #A5A5Af;
+    color: #a5a5af;
   }
 `;
 
 const Line = styled.span`
-width: 2px;
-height: 6rem;
-margin-top: 0.5em;
-background-color: ${props => props.theme.text};
-`
+  width: 2px;
+  height: 6rem;
+  margin-top: 0.5em;
+  background-color: ${(props) => props.theme.text};
+`;
 
-let SOCIAL_SIZE = '1.5rem'
+let SOCIAL_SIZE = "1.5rem";
 
 export const Socials = () => {
   return (
     <>
       <Icons>
-        <a href={"https://github.com/bmaurice98"} target={'_blank'}>
+        <a
+          href={"https://github.com/bmaurice98"}
+          target={"_blank"}
+          rel="noreferrer"
+        >
           <SiGithub fontSize={SOCIAL_SIZE} />
         </a>
-        <a href={"https://www.linkedin.com/in/brandon-maurice-a77190187/"} target={'_blank'}>
+        <a
+          href={"https://www.linkedin.com/in/brandon-maurice-a77190187/"}
+          target={"_blank"}
+          rel="noreferrer"
+        >
           <SiLinkedin fontSize={SOCIAL_SIZE} />
         </a>
-        <a href={"https://leetcode.com/MizuPiku/"} target={'_blank'}>
+        <a
+          href={"https://leetcode.com/MizuPiku/"}
+          target={"_blank"}
+          rel="noreferrer"
+        >
           <SiLeetcode fontSize={SOCIAL_SIZE} />
         </a>
-        <a href={"https://www.twitch.tv/mizupiku"} target={'_blank'}>
+        <a
+          href={"https://www.twitch.tv/mizupiku"}
+          target={"_blank"}
+          rel="noreferrer"
+        >
           <SiTwitch fontSize={SOCIAL_SIZE} />
         </a>
-        <a href={"https://open.spotify.com/user/bmaurice100"} target={'_blank'}>
+        <a
+          href={"https://open.spotify.com/user/bmaurice100"}
+          target={"_blank"}
+          rel="noreferrer"
+        >
           <SiSpotify fontSize={SOCIAL_SIZE} />
         </a>
         <Line />
