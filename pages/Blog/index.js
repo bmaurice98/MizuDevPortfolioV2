6f1a6107484c572a3d05b74d5d6d85a1d3da367a
Brandon <bmaurice100@gmail.com>
2022-07-19 21:29:33 -0700

    Building Blog page

--- pages/Blog/index.js
+++ pages/Blog/index.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { HomeButton } from "../../components/HomeButton";
+import { LogoComponent } from "../../components/LogoComponent";
+import { Socials } from "../../components/Socials";
+
+const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba}, 1)`};
+  width: 100%;
+  height: 100%;
+  position: relative;
+  padding-bottom: 5rem;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
+`;
 
 const Blog = () => {
   return (
-    <div>BlogPage</div>
-  )
-}
+    <MainContainer>
+      <Container>
+        <HomeButton />
+        <LogoComponent />
+        <Socials />
+      </Container>
+      <Center>
+        <Grid></Grid>
+      </Center>
+    </MainContainer>
+  );
+};
 
-export default Blog
+export default Blog;
