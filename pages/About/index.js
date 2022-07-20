ce1060e880b514625dac961de49fceb893ca2c01
Brandon <bmaurice100@gmail.com>
2022-07-19 22:33:48 -0700

    Setup each pages base layout

--- pages/About/index.js
+++ pages/About/index.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { HomeButton } from "../../components/HomeButton";
+import { LogoComponent } from "../../components/LogoComponent";
+import { Socials } from "../../components/Socials";
+
+const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba}, 1)`};
+  width: 100%;
+  height: 100%;
+  position: relative;
+  padding-bottom: 5rem;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <MainContainer>
+      <Container>
+        <HomeButton />
+        <LogoComponent />
+        <Socials />
+      </Container>
+      <Center></Center>
+    </MainContainer>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
