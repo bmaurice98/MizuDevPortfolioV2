2fe2340ce8e4ce17071dc02479e7b029eebeecd6
Brandon <bmaurice100@gmail.com>
2022-07-24 23:12:34 -0700

    commiting before the day ends

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -17,7 +17,7 @@ const PostDetails = ({ post }) => {
   }
 
   return (
-    <div className="container mx-auto px-10 mb-8 mt-10">
+    <div className="">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="cols-span-1 lg:col-span-8">
           <PostDetail post={post} />
