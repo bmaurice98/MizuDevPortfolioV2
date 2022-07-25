d6b36d7acc5561eda72154559578d8a1afe68f67
Brandon <bmaurice100@gmail.com>
2022-07-25 13:29:05 -0700

    _document.js file for to apply fonts and styles for all pages

--- pages/index.js
+++ pages/index.js
@@ -8,21 +8,6 @@ export default function Home() {
         <title>MizuDev Personal</title>
         <meta name="description" content="My Web Developer Portfolio" />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          crossOrigin="true"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;500;600&family=Pacifico&family=Source+Sans+Pro:wght@400;600;700&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400&display=swap"
-          rel="stylesheet"
-          crossOrigin="true"
-        />
       </Head>
 
       <Main />
