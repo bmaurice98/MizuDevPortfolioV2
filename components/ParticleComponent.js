206f61a5358f7524150dd8fd63438e2d196bb4ca
Brandon <bmaurice100@gmail.com>
2022-08-08 21:13:07 -0700

    Importing particles.
    Reactparticles is deprecated, so time to learn tsParticles

--- components/ParticleComponent.js
+++ components/ParticleComponent.js
@@ -14,10 +14,15 @@ const Box = styled.div`
   z-index: 0;
 `;
 
-const ParticleComponent = () => {
-  return <Box>
-    <Particles
-  </Box>;
+const ParticleComponent = (props) => {
+  return (
+    <Box>
+      <Particles
+        style={{ position: "absolute", top: 0 }}
+        params={props.theme === "light" ? configLight : configDark}
+      />
+    </Box>
+  );
 };
 
 export default ParticleComponent;
