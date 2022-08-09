6edc37f3b62145e0cc625355530297cac7b9ba0a
Brandon <bmaurice100@gmail.com>
2022-08-08 20:14:29 -0700

    Building the skills page

--- pages/Skills/index.js
+++ pages/Skills/index.js
@@ -1,39 +1,57 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { HomeButton } from "../../components/HomeButton";
 import { LogoComponent } from "../../components/LogoComponent";
 import { Socials } from "../../components/Socials";
+import { lightTheme } from "../../components/Themes";
+import { Design, Develope } from "";
 
 const MainContainer = styled.div`
   width: 100vw;
   height: 100vh;
-`;
-
-const Container = styled.div`
   background-color: ${(props) => `rgba(${props.theme.bodyRgba}, 1)`};
-  width: 100%;
-  height: 100%;
   position: relative;
-  padding-bottom: 5rem;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
 `;
 
-const Center = styled.div`
+const Container = styled.div``;
+
+const Main = styled.div`
+  width: 30vw;
+  height: 60vh;
+  padding: 2rem;
+  border: 2px solid ${(props) => props.theme.text};
+  justify-content: center;
+  align-items: center;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Title = styled.h2`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-top: 10rem;
+  font-size: calc(1em + 1vw);
 `;
 
 const SkillsPage = () => {
   return (
-    <MainContainer>
-      <Container>
+    <ThemeProvider theme={lightTheme}>
+      <MainContainer>
+        <Main>
+          <Title></Title>
+        </Main>
+        <Main>2</Main>
         <HomeButton />
         <LogoComponent />
         <Socials />
-      </Container>
-      <Center></Center>
-    </MainContainer>
+      </MainContainer>
+    </ThemeProvider>
   );
 };
 
