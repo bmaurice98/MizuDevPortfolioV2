03511ac82d75224477a4d8a34f2131bd9bff91f6
Brandon <bmaurice100@gmail.com>
2022-08-14 21:36:42 -0700

    Next-seo imlpemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,31 @@
 import React from "react";
+import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "styled-components";
 import { lightTheme } from "../components/Themes";
 import Transition from "../components/Transition";
 import "../styles/globals.css";
 import "../styles/transition.css";
 import "tailwindcss/tailwind.css";
-import PreLoader from "../components/PreLoader";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Transition>
-      <ThemeProvider theme={lightTheme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Transition>
+    <>
+      <DefaultSeo
+        titleTemplate="%s - Brandon Maurice"
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          description: "The personal website for Brandon Maurice, developer.",
+          site_name: "Brandon Maurice | devmizu.tech",
+          images: [],
+        }}
+      />
+      <Transition>
+        <ThemeProvider theme={lightTheme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Transition>
+    </>
   );
 }
 
